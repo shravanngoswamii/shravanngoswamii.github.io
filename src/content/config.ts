23021f7d2397ae63ca7a0b805e875bcd790545ac
Shravan Goswami <shravanngoswamii@gmail.com>
2024-10-08 21:12:24 +0530

    updated date format

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,21 +2,14 @@ import { SITE } from "@config";
 import { glob } from "astro/loaders";
 import { defineCollection, z } from "astro:content";
 
-// Function to parse dd-mm-yyyy format
-const parseDateString = (dateString: string | null | undefined): Date | null => {
-  if (!dateString) return null; // Handle null or undefined
-  const [day, month, year] = dateString.split('-');
-  return new Date(`${year}-${month}-${day}T00:00:00Z`); // Convert to ISO format
-};
-
 const blog = defineCollection({
   type: "content_layer",
   loader: glob({ pattern: "**/*.md", base: "./src/content/blog" }),
   schema: ({ image }) =>
     z.object({
       author: z.string().default(SITE.author),
-      pubDatetime: z.string().transform(parseDateString).refine(date => date !== null && !isNaN(date.getTime())),
-      modDatetime: z.string().optional().nullable().transform(parseDateString).refine(date => date === null || !isNaN(date.getTime())),
+      pubDatetime: z.date(),
+      modDatetime: z.date().optional().nullable(),
       title: z.string(),
       featured: z.boolean().optional(),
       draft: z.boolean().optional(),
