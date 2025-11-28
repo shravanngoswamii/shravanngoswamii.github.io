fdbecf97b323d6b94faddb49b3164eafb5e536c5
Shravan Goswami <shravanngoswamii@gmail.com>
2025-11-28 15:48:09 +0530

    Add ESLint, fix linting issues and update project layout

---
+++ eslint.config.mjs
@@ -0,0 +1,36 @@
+import eslintPluginAstro from 'eslint-plugin-astro';
+import tseslint from 'typescript-eslint';
+
+export default [
+  {
+    ignores: [
+        "dist/",
+        "node_modules/",
+        ".github/",
+        ".astro/",
+        "public/",
+        "src/env.d.ts",
+        "**/*.d.ts",
+        "src/pages/\\[...alias\\].astro"
+    ],
+  },
+  
+  ...tseslint.configs.recommended,
+  ...eslintPluginAstro.configs.recommended,
+
+  {
+    files: ["**/*.cjs"],
+    rules: {
+      "@typescript-eslint/no-require-imports": "off",
+      "@typescript-eslint/no-var-requires": "off"
+    }
+  },
+
+  {
+    files: ["**/*.{js,mjs,cjs,ts,tsx}"],
+    rules: {
+      "prefer-rest-params": "error",
+      "no-var": "error"
+    }
+  }
+];
